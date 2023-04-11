0d78abd1339aadd184cbc6c5ee52afbf94601633
TheLastMelancholy <uadrugusserko@yandex.ru>
2023-04-11 10:19:24 +0300

    ref: vim tmp's files removed

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ PLANNING_SCHEMA = "__daily_example.json"
 #  DAILY_PLAN = ["morning", "working", "working", "learning",
 #                  "learning", "midday", "learning", "learning",
 #                  "learning", "evening"]
+#  PLANNING_SCHEMA = "daily.json"
